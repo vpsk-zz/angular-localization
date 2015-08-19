8c74f943ad945b814182b6cd3de559a463f182bd
virendra <vpsk0505@gmail.com>
2015-08-19 23:57:51 +0530

    first commit project seed

---
+++ src/app/index.constants.js
@@ -0,0 +1,11 @@
+/* global malarkey:false, toastr:false, moment:false */
+(function() {
+  'use strict';
+
+  angular
+    .module('myNewProject')
+    .constant('malarkey', malarkey)
+    .constant('toastr', toastr)
+    .constant('moment', moment);
+
+})();
