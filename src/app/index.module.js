27bdd99da524742671ecf0234d9f698d5d947714
virendra <vpsk05@gmail.com>
2015-10-18 03:20:32 +0530

    changes

--- src/app/index.module.js
+++ src/app/index.module.js
@@ -2,6 +2,6 @@
   'use strict';
 
   angular
-    .module('myNewProject', ['ngAnimate', 'ngCookies', 'ngTouch', 'ngSanitize', 'ngResource', 'ui.router', 'ui.bootstrap']);
+    .module('myNewProject', ['ngAnimate', 'ngCookies', 'ngTouch', 'ngSanitize', 'ngResource', 'ui.router', 'ui.bootstrap','localizationModule']);
 
 })();
